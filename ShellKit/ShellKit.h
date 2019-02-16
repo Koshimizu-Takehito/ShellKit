5d1b78807e7a8c8c64c31ea6427eef1973875fb9
takehito koshimizu <takehito.koshimizu@gmail.com>
2019-02-16 14:13:15 +0900

    Initial Commit

---
+++ ShellKit/ShellKit.h
@@ -0,0 +1,13 @@
+//
+//  ShellKit.h
+//  ShellKit
+//
+//  Created by Takehito Koshimizu on 2019/02/16.
+//  Copyright © 2019 Takehito Koshimizu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShellKit : NSObject
+
+@end
---
+++ ShellKit/ShellKit.m
@@ -0,0 +1,13 @@
+//
+//  ShellKit.m
+//  ShellKit
+//
+//  Created by Takehito Koshimizu on 2019/02/16.
+//  Copyright © 2019 Takehito Koshimizu. All rights reserved.
+//
+
+#import "ShellKit.h"
+
+@implementation ShellKit
+
+@end
